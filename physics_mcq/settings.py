2b4df78ef5baacca9335f3f87ccac6d5ef192453
Robert Treharne <treharne@liverpool.ac.uk>
2025-04-18 10:34:58 +0100

    tell production to use postgres

--- physics_mcq/settings.py
+++ physics_mcq/settings.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import os
+import dj_database_url
 from dotenv import load_dotenv
 load_dotenv()
 
@@ -99,10 +100,9 @@ WSGI_APPLICATION = 'physics_mcq.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}"
+    )
 }
 
 
