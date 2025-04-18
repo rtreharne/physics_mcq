531b76fec20e2a4663585a22b6ae6ca68f7a3f70
Robert Treharne <treharne@liverpool.ac.uk>
2025-04-18 07:57:29 +0100

    .env updated

--- physics_mcq/settings.py
+++ physics_mcq/settings.py
@@ -12,12 +12,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^)nl#x=3r-ueh@ru(f4=758y*#ldhbp-!^j%nup$(8nj^+ip_k'
+SECRET_KEY = os.getenv('SECRET_KEY', default='django-insecure-!@#4$%^&*()_+')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', default='True') == 'True'
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', default='localhost').split(',')
 
-ALLOWED_HOSTS = []
 
 
 # Application definition
